cb8dc2a4bdbf3ea75ef5fced56461a5f346205ca
tomakpro0518-star <tomak.pro.0518@gmail.com>
2025-11-24 06:09:51 -0800

    second

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.end('Hello!');
 }).listen(3000);
 
 console.log("Server is running at 3000")
