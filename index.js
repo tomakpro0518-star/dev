7144ef8790ff234d04a9f813f2abb7f6f7dba279
tomakpro0518-star <tomak.pro.0518@gmail.com>
2025-11-24 20:31:08 -0800

    dddd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello! This is my first DevOps Project');
+  res.end('Hello world!');
 }).listen(3000);
 
 console.log("Server is running at 3000")
